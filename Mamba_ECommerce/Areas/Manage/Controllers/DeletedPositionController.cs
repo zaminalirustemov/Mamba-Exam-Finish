4088275e382808e615e964b18669b80cf47da119
emilhajizade42 <6xp3xcg@code.edu.az>
2023-01-28 10:59:54 +0400

    Mamba-Exam

---
+++ Mamba_ECommerce/Areas/Manage/Controllers/DeletedPositionController.cs
@@ -0,0 +1,46 @@
+﻿using Mamba_ECommerce.Context;
+using Mamba_ECommerce.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Mamba_ECommerce.Areas.Manage.Controllers;
+[Area("Manage")]
+[Authorize(Roles = "SuperAdmin,Admin,Editor")]
+public class DeletedPositionController : Controller
+{
+    private readonly MambaDbContext _mambaDbContext;
+
+    public DeletedPositionController(MambaDbContext mambaDbContext)
+    {
+        _mambaDbContext = mambaDbContext;
+    }
+    //Read---------------------------------------------------------------------------------------------------------------
+    public IActionResult Index()
+    {
+        List<Position> positions = _mambaDbContext.Positions.Where(d => d.isDeleted == true).ToList();
+        return View(positions);
+    }
+    //Restore---------------------------------------------------------------------------------------------------------
+    public IActionResult Restore(int id)
+    {
+        Position position = _mambaDbContext.Positions.FirstOrDefault(x => x.Id == id);
+        if (position == null) return View("Error-404");
+
+        position.isDeleted = false;
+        _mambaDbContext.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+    //HardDelete---------------------------------------------------------------------------------------------------------
+    public IActionResult HardDelete(int id)
+    {
+        Position position = _mambaDbContext.Positions.FirstOrDefault(x => x.Id == id);
+        if (position == null) return View("Error-404");
+
+        _mambaDbContext.Positions.Remove(position);
+        _mambaDbContext.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Mamba_ECommerce/Areas/Manage/Controllers/PositionController.cs
@@ -0,0 +1,70 @@
+﻿using Mamba_ECommerce.Context;
+using Mamba_ECommerce.Helpers;
+using Mamba_ECommerce.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Mamba_ECommerce.Areas.Manage.Controllers;
+[Area("Manage")]
+[Authorize(Roles = "SuperAdmin,Admin,Editor")]
+public class PositionController : Controller
+{
+    private readonly MambaDbContext _mambaDbContext;
+
+    public PositionController(MambaDbContext mambaDbContext)
+    {
+        _mambaDbContext = mambaDbContext;
+    }
+    //Read---------------------------------------------------------------------------------------------------------------
+    public IActionResult Index()
+    {
+        List<Position> positions = _mambaDbContext.Positions.Where(d => d.isDeleted == false).ToList();
+        return View(positions);
+    }
+    //Create-------------------------------------------------------------------------------------------------------------
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Position position)
+    {
+        if (!ModelState.IsValid) return View(position);
+
+        _mambaDbContext.Positions.Add(position);
+        _mambaDbContext.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+    //Update-------------------------------------------------------------------------------------------------------------
+    public IActionResult Update(int id)
+    {
+        Position position = _mambaDbContext.Positions.FirstOrDefault(x => x.Id == id);
+        if (position == null) return View("Error-404");
+
+        return View(position);
+    }
+    [HttpPost]
+    public IActionResult Update(Position newPosition)
+    {
+        Position existPosition = _mambaDbContext.Positions.FirstOrDefault(x => x.Id == newPosition.Id);
+        if (existPosition == null) return View("Error-404");
+        if (!ModelState.IsValid) return View(existPosition);
+
+        existPosition.Name = newPosition.Name;
+        _mambaDbContext.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+    //SoftDelete---------------------------------------------------------------------------------------------------------
+    public IActionResult SoftDelete(int id)
+    {
+        Position position = _mambaDbContext.Positions.FirstOrDefault(x => x.Id == id);
+        if (position == null) return View("Error-404");
+
+        position.isDeleted = true;
+        _mambaDbContext.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
